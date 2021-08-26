6a0fbdcfca4c5942065583125064bb6b564494d2
Sean Cogley <smcogley@gmail.com>
2021-08-26 15:46:25 -0700

    get method returns new guid

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiNetCoreProject2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return System.Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiNetCoreProject2.Models
+{
+    public class UserModel
+    {
+        // User Id(service or database auto-generated guid)
+
+        // User Email(required)
+        // User Password(required) store it in plain text or better yet hashed
+        // Created Date(service generated)
+    }
+}

01e266e684ccd89ee52eaadf8a20416cc3738271
Sean Cogley <smcogley@gmail.com>
2021-09-02 16:43:56 -0700

    add tests for post, put, delete

--- tests/ApiNetCoreProject2.Tests/UserControllerTest.cs
+++ tests/ApiNetCoreProject2.Tests/UserControllerTest.cs
@@ -33,7 +33,7 @@ namespace ApiNetCoreProject2.Tests
         public UserModel AddTestUser()
         {   
             UserModel user = new UserModel();
-            user.Email = $"testuser@gmail.com";
+            user.Email = "testuser@gmail.com";
             user.Password = "foobar";
             var newUser = _service.Add(user);
             return newUser;
@@ -76,6 +76,77 @@ namespace ApiNetCoreProject2.Tests
             var result = _controller.Get();
 
             result.Should().BeOfType<List<UserModel>>();
+        }
+
+        [TestMethod]
+        public void Post_WhenCalled_ReturnsCreated()
+        {
+            // Arrange: create a User that would be passed in message body
+            UserModel value = new UserModel();
+            value.Email = "testuser@gmail.com";
+            value.Password = "foobar";
+
+            // Act: call Post
+            var result = (CreatedAtActionResult)_controller.Post(value);
+
+            result.Should().BeOfType<CreatedAtActionResult>();
+        }
+
+        [TestMethod]
+        public void Put_WhenCalledWithInvalidGuid_ReturnsNotFoundResult()
+        {
+            // Arrange: create a user value
+            UserModel value = new UserModel();
+            value.Email = "testuser@gmail.com";
+            value.Password = "foobar";
+
+            // Act: call Put with the user value to update
+            var result = (NotFoundResult)_controller.Put(new System.Guid(), value);
+
+            // Assert: 404 Not Found since the guid passed was not found in "db".
+            //Assert.AreEqual(result.StatusCode, new NotFoundResult().StatusCode);
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [TestMethod]
+        public void Put_WhenCalledWithValidGuid_ReturnsOk()
+        {
+            // Arrange: add new user directly using _service.Add() and retrieve the resulting UserId.
+            var newUser = AddTestUser();
+
+            // Act: call Put and pass the valid UserId.
+            var result = (OkObjectResult)_controller.Put(newUser.UserId, newUser);
+
+            // Assert: Ok Object Result Code
+            //Assert.AreEqual(result.StatusCode, new OkObjectResult("guid").StatusCode);
+
+            result.StatusCode.Should().Be(new OkObjectResult("guid").StatusCode);
+        }
+
+        [TestMethod]
+        public void Delete_WhenCalledWithValidGuid_ReturnsOk()
+        {
+            // Arrange: add new user directly using _service.Add() and retrieve the resulting UserId.
+            var newUser = AddTestUser();
+
+            // Act: call Put and pass the valid UserId.
+            var result = (OkResult)_controller.Delete(newUser.UserId);
+
+            // Assert: Ok Object Result Code
+            result.StatusCode.Should().Be(new OkResult().StatusCode);
+        }
+
+        [TestMethod]
+        public void Delete_WhenCalledWithInvalidGuid_ReturnsNotFoundObjectResult()
+        {
+            // Arrange: add new user directly using _service.Add() and retrieve the resulting UserId.
+            var newUser = AddTestUser();
+
+            // Act: call Delete and pass a newly generated guid (invalid) UserId.
+            var result = (NotFoundObjectResult)_controller.Delete(new System.Guid());
+
+            // Assert: NotFoundObjectResult Code
+            result.StatusCode.Should().Be(new NotFoundObjectResult("foo").StatusCode);
         }
     }
 }
